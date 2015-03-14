7399033368531e232194c0c40136d430957086df
Paul Mucur <mudge@mudge.name>
2015-03-14 16:46:02 +0000

    Complete basic test suite

--- tests/MongoSessionHandlerTest.php
+++ tests/MongoSessionHandlerTest.php
@@ -6,37 +6,92 @@ use Psr\Log\NullLogger;
 
 class MongoSessionHandlerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testOpen()
+    public function testOpenReturnsTrue()
     {
         $handler = $this->buildHandler();
 
         $this->assertTrue($handler->open('/some/path', 'foo'));
     }
 
-    public function testClose()
+    public function testCloseReturnsTrue()
     {
         $handler = $this->buildHandler();
 
         $this->assertTrue($handler->close());
     }
 
-    public function testReadExistingSession()
+    public function testReadExistingSessionReturnsTheData()
     {
         $collection = $this->mockMongoCollection();
         $collection->method('findOne')
-                   ->willReturn(['_id' => '123', 'data' => new \MongoBinData('foo', \MongoBinData::BYTE_ARRAY)]);
+                   ->willReturn(['_id' => '123',
+                                 'data' => new \MongoBinData('foo', \MongoBinData::BYTE_ARRAY)]);
         $handler = $this->buildHandler($collection);
 
         $this->assertEquals('foo', $handler->read('123'));
     }
 
-    public function testReadMissingSession()
+    public function testReadMissingSessionReturnsAnEmptyString()
     {
         $collection = $this->mockMongoCollection();
         $collection->method('findOne')->willReturn(null);
         $handler = $this->buildHandler($collection);
 
         $this->assertEquals('', $handler->read('123'));
+    }
+
+    public function testWriteSuccessfullyReturnsTrue()
+    {
+        $collection = $this->mockMongoCollection();
+        $collection->method('save');
+        $handler = $this->buildHandler($collection);
+
+        $this->assertTrue($handler->write('123', serialize(['user_id' => 1])));
+    }
+
+    public function testWriteUnsuccessfullyReturnsFalse()
+    {
+        $collection = $this->mockMongoCollection();
+        $collection->method('save')->will($this->throwException(new \MongoException));
+        $handler = $this->buildHandler($collection);
+
+        $this->assertFalse($handler->write('123', serialize(['user_id' => 1])));
+    }
+
+    public function testDestroySuccessfullyReturnsTrue()
+    {
+        $collection = $this->mockMongoCollection();
+        $collection->method('remove');
+        $handler = $this->buildHandler($collection);
+
+        $this->assertTrue($handler->destroy('123'));
+    }
+
+    public function testDestroyUnsuccessfullyReturnsFalse()
+    {
+        $collection = $this->mockMongoCollection();
+        $collection->method('remove')->will($this->throwException(new \MongoException));
+        $handler = $this->buildHandler($collection);
+
+        $this->assertFalse($handler->destroy('123'));
+    }
+
+    public function testGcSuccessfullyReturnsTrue()
+    {
+        $collection = $this->mockMongoCollection();
+        $collection->method('remove');
+        $handler = $this->buildHandler($collection);
+
+        $this->assertTrue($handler->gc(14400));
+    }
+
+    public function testGcUnsuccessfullyReturnsFalse()
+    {
+        $collection = $this->mockMongoCollection();
+        $collection->method('remove')->will($this->throwException(new \MongoException));
+        $handler = $this->buildHandler($collection);
+
+        $this->assertFalse($handler->gc(14400));
     }
 
     private function buildHandler($collection = null)
